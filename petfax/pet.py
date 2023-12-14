ab6d9931094dcd5aa244152cfe7f8c457e171220
JDiaz <jdiaz_007@outlook.com>
2023-12-13 22:54:49 -0800

    created show route in pet.py file and created show.html file (only shows pet name so far)

--- petfax/pet.py
+++ petfax/pet.py
@@ -26,6 +26,11 @@ bp = Blueprint(
 # Defines a method for the route named index that returns a string
 def index():
     return render_template (
-        'index.html',
+        'pets/index.html',
         pets=pets,
         )
+
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('pets/show.html', pet=pet)
