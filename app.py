79565baba2e208c0fdacb90b17f6cefb876c2de8
JDiaz <jdiaz_007@outlook.com>
2023-12-13 19:52:08 -0800

    imported create_app from petfax folder and created an app instance in the app.py file

---
+++ app.py
@@ -0,0 +1,5 @@
+# *** Imports create_app from petfax folder
+from petfax import create_app
+
+# * Creates an app instance in the app.py file by invoking the function and saving it to a variable called app
+app = create_app()
