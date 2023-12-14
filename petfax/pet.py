512b4e6d3e3f6e4c1b7f46dc222186371776dcb8
JDiaz <jdiaz_007@outlook.com>
2023-12-13 20:57:58 -0800

    added blueprint route in pet.py file

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,7 +1,18 @@
+# *** Imports the blueprint class
 from flask import Blueprint
 
+# * Creates an instance of the blueprint class
 bp = Blueprint(
+    # Defines the bludeprint name
     'pet',
+    # Tell the blueprint where in the project it's defined
     __name__,
+    # Defines the blueprint's url prefix
     url_prefix ='/pets'
 )
+
+# * Defines a route on the blueprint instance that goes to '/'
+@bp.route('/')
+# Defines a method for the route named index that returns a string
+def index():
+    return 'You have reached the pets index =D'
