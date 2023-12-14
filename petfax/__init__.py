879175a81a6b9b9c060dfaf624b5f81f9dd9d568
JDiaz <jdiaz_007@outlook.com>
2023-12-13 21:09:35 -0800

    registered the blueprint in the application factory (__init__.py)

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,7 @@
 # *** Imports the flask package as Flask
 from flask import Flask
 
-# * Defines function that will be the application factory
+# * Defines a function that will be the application factory
 def create_app():
     # * App instance of flask
     app = Flask(__name__)
@@ -10,5 +10,10 @@ def create_app():
     @app.route('/')
     def hello():
         return "Hello, PetFax! 🐶"
-    
+
+    #Imports the pet file
+    from . import pet
+    # Calls the register_blueprint method on the app instance and passes the pet blueprint into the method
+    app.register_blueprint(pet.bp)
+
     return app
